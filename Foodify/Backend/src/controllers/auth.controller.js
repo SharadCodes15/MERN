3b9e0781ac52119f1ecbe8f010da866292995a2c
SharadCodes15 <codebysharad@gmail.com>
2026-09-14 06:25:11 +0530

    [FoodView -> Backend] Login Auth

--- Foodify/Backend/src/controllers/auth.controller.js
+++ Foodify/Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+
 async function registerUser(req,res) {
     const {fullname,email,password} = req.body;
      
@@ -36,6 +37,39 @@ async function registerUser(req,res) {
     }) 
 }
 
+async function loginUser(req,res) {
+     const {email,password} = req.body;
+
+     const user = await userModel.findOne({
+        email
+     })
+
+     if(!user){
+        return res.status(400).json({
+            message:"Invalid email or password"
+        })
+     }
+     const isPasswordValid = await bcrypt.compare(password,user.password)
+     if (!isPasswordValid){
+        return res.status(400).json({
+            message:"Invalid email or password"
+        })
+     }
+    const token = jwt.sign({
+        id:user._id,
+    },"e8c2w3bQpUz5XkDFJbtLuO6l3narGAHc")
+    res.cookie("token",token);
+    res.status(201).json({
+        message:"User Registered Successfully",
+        user:{
+            _id:user._id,
+            email:user.email,
+            fullname:user.fullname
+        }
+    }) 
+}
+
 module.exports = {
     registerUser,
+    loginUser,
 }
--- Foodify/Backend/src/routes/auth.routes.js
+++ Foodify/Backend/src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 
 router.post('/user/register',authController.registerUser)
+router.post('/user/login',authController.loginUser)
 
 module.exports = router
