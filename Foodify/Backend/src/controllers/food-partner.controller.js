3f3716588f67e28745eb18f0173a35d790c2916c
SharadCodes15 <codebysharad@gmail.com>
2026-09-23 03:30:44 +0530

    [FoodView -> FULL] FoodItems By FoodPartner

--- Foodify/Backend/src/controllers/food-partner.controller.js
+++ Foodify/Backend/src/controllers/food-partner.controller.js
@@ -1,15 +1,19 @@
 const foodpartnerModel = require('../models/foodpartner.model');
-
+const foodModel = require('../models/food.model');
 async function getFoodPartnerByID(req,res) {
     const foodPartnerId = req.params.id;
     const foodPartner = await  foodpartnerModel.findById(foodPartnerId)
+    const foodItemsByFoodPartner = await foodModel.find({foodPartner:foodPartnerId})
     if(!foodPartner){
         return res.status(404).json({message:"Food Partner not found"})
     }
     res.status(200).json({
         message:"Food patner retrieved successfully",
-        foodPartner
-    })
+        foodPartner:{
+            ...foodPartner.toObject(),
+            foodItems:foodItemsByFoodPartner
+        }
+    }) 
 }
 
 module.exports = {
