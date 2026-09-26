ed2ed217f1db2673c7d387a7ef9c8a23f4c2acf7
SharadCodes15 <codebysharad@gmail.com>
2026-09-26 08:49:46 +0530

    [FoodView -> Frontend] LandingPage Layout Changes

--- Foodify/Frontend/src/pages/Landing/Landingpage.jsx
+++ Foodify/Frontend/src/pages/Landing/Landingpage.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import LandingNavbar from './LandingNavbar';
-
+import VideoSection from './VideoSection'
+import HeroLand from './HeroLand';
 const Landingpage = () => {
   return (
     <div>
       <main>
-        <section className="bg-white">
-            <LandingNavbar/>
-          <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-            <div className="mx-auto max-w-3xl text-center">
-              <h1 className="bg-gradient-to-r from-[#FF6B6B] to-[#FFD93D] bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-                Welcome to the Neighborhood Culinary Network
-              </h1>
-            </div>
+        <HeroLand/>
+        {/* <VideoSection/> */}
+        <section className="h-screen bg-black">
+          <VideoSection/>
+        </section>  
+      <section className="h-screen bg-amber-400">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="bg-gradient-to-r from-[#FF6B6B] to-[#FFD93D] bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Explore the World of Culinary Delights
+            </h1>
           </div>
-        </section>
+        </div>
+      </section>
+
       </main>
 
     </div>
