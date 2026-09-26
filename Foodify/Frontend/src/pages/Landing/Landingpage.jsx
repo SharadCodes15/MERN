fe255aa655b46523194b6cca8856859d16dd04a3
SharadCodes15 <codebysharad@gmail.com>
2026-09-26 11:04:34 +0530

    [FoodView -> Frontend] Landing -> Page Final

--- Foodify/Frontend/src/pages/Landing/Landingpage.jsx
+++ Foodify/Frontend/src/pages/Landing/Landingpage.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import LandingNavbar from './LandingNavbar';
-import VideoSection from './VideoSection'
-import HeroLand from './HeroLand';
-import './Landing.css'
+import React, { useRef, useLayoutEffect } from "react";
+import LandingNavbar from "./LandingNavbar";
+import VideoSection from "./VideoSection";
+import HeroLand from "./HeroLand";
+import Lenis from "lenis";
+import "./Landing.css";
+import SmoothScroll from "./SmoothScroll";
+import ItemDisplay from "./ItemDisplay";
+import FinalCTA from "./FinalCTA";
+import Footer from "./Footer";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// gsap.registerPlugin(ScrollTrigger);
 const Landingpage = () => {
   return (
-    <div>
-      <main>
-        
-        <HeroLand/>
-        {/* <VideoSection/> */}
-        <section className="h-screen bg-black">
-          <VideoSection/>
-        </section>  
-      <section className="h-screen bg-amber-400">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-          <div className="mx-auto max-w-3xl text-center">
-            <h1 className="bg-gradient-to-r from-[#FF6B6B] to-[#FFD93D] bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-              Explore the World of Culinary Delights
-            </h1>
-          </div>
-        </div>
-      </section>
+    <SmoothScroll>
+      <div>
+        <main>
+          <HeroLand />
+          {/* <VideoSection/> */}
+          <VideoSection />
+          <ItemDisplay/>
+          <FinalCTA/>
+          <Footer/>
+        </main>
+      </div>
+    </SmoothScroll>
+  );
+};
 
-      </main>
-
-    </div>
-  )
-}
-
-export default Landingpage
+export default Landingpage;
